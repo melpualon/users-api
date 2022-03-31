68bf88433c18e7ee9a6639e4a754fb0f5b65e53e
melpualon <mel@xperto.ph>
2022-03-31 14:28:24 +0800

    added new page

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <Nav />
+    <div class="w-full h-screen flex items-center justify-center">
+        <h1 class="text-3xl">This is about page</h1>
+    </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue'
+export default {
+    components: { Nav }
+}
+</script>
+
+<style>
+
+</style>
